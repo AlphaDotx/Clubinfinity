54b65237baf77a2f3c4e5996000af3ea46a84bfd
Sir Williams <wesley@work.com>
2020-01-12 20:25:56 +0200

    added chat on register

--- register.php
+++ register.php
@@ -111,7 +111,19 @@ if(isset($_POST['btn-signup']))
   <link href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
   <!-- Argon CSS -->
   <link type="text/css" href="assets/css/argon.css?v=1.0.0" rel="stylesheet">
-
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e16c30c7e39ea1242a3bdb2/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </head>
 
 <body class="bg-default">
